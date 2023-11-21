90c53382f4b0c25f8cebb95fb07761fa823e866a
TommyTheMob <zv3rbrus@gmail.com>
2023-11-21 16:10:29 +0300

    base wepback build complete

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,24 @@
+import {Configuration} from "webpack"
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/types";
+
+export function buildPlugins(options: BuildOptions): Configuration['plugins'] {
+
+    const isProd = options.mode === 'production'
+
+    const plugins: Configuration['plugins'] = [
+        new HtmlWebpackPlugin({template: options.paths.html})
+    ]
+
+    if (isProd) {
+        plugins.push(
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css'
+            })
+        )
+    }
+
+    return plugins
+}
---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import classes from './App.module.scss'
+
+export const App = () => {
+    const [count, setCount] = useState<number>(0)
+
+    const increment = () => setCount(prev => prev + 1)
+
+    return (
+        <div>
+            Hello World
+            <h1 className={classes.value}>{count}</h1>
+            <button className={classes.button} onClick={increment}>inc</button>
+        </div>
+    );
+};
